258de2537c9210fdc12f3059dad84789ee404999
Aakash Singh <akash936924@gmail.com>
2026-09-04 04:55:21 +0530

    Update objects.ts

---
+++ 03-Objects/Basic Object.ts
@@ -0,0 +1,46 @@
+// const student = {
+//   name: "Aakash",
+//   age: 21,
+//   isPlaced: false
+// };
+
+// console.log(student.name); // Aakash
+// console.log(student.age);  // 21
+
+
+//Object Type Annotation
+// const student: {
+//   name: string;
+//   age: number;
+//   isPlaced: boolean;
+// } = {
+//   name: "Aakash",
+//   age: 21,
+//   isPlaced: false
+// };
+
+// const student: {
+//   name: string;
+//   age: number;
+// } = {
+//   name: "Aakash",
+//   age: "21"
+// };
+
+
+// const employee = {
+//   name: "Aakash",
+//   address: {
+//     city: "Prayagraj",
+//     state: "UP"
+//   }
+// };
+
+// console.log(employee.address.city);
+
+
+// const student = {
+//     name: "Aakash",
+//     age: 21,
+//     isPlaced: false
+// };
