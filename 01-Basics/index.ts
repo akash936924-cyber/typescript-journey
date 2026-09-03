979321e110a80e7fb03731f53374d2c0ae51e9d2
Aakash Singh <akash936924@gmail.com>
2026-09-03 14:56:00 +0530

    Day 1: TypeScript Basics (Variables & Basic Types)

---
+++ 01-Basics/index.ts
@@ -0,0 +1,37 @@
+// Variables
+
+// let firstName: string = "Aakash";
+// let age: number = 21;
+// let isStudent: boolean = true;
+
+// console.log(firstName);
+// console.log(age);
+// console.log(isStudent);
+
+
+// Day 1 - Variables & Basic Types
+
+let firstName: string = "Aakash";
+let age: number = 21;
+let isStudent: boolean = true;
+
+console.log("Name:", firstName);
+console.log("Age:", age);
+console.log("Student:", isStudent);
+
+// let vs const
+
+let city: string = "Lucknow";
+city = "Delhi";
+
+const country: string = "India";
+
+// country = "USA"; // Error
+
+console.log("City:", city);
+console.log("Country:", country);
+
+// Type Inference
+
+let company = "Microsoft";
+console.log("Company:", company);
